6708ed3dc2dd7d6bc6c00e6f7204e063aea20888
Евгений Куликов <zh.culikov58@outlook.com>
2024-10-23 20:26:52 +0300

    start

---
+++ Batiskaf/Submarine.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Batiskaf
+{
+    public partial class Submarine : UserControl
+    {
+        private Image backgroundImage;
+        public Submarine()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+            LoadBackgroundImage();
+        }
+
+
+        private void LoadBackgroundImage()
+        {
+            try
+            {
+                backgroundImage = Image.FromFile("батискаф.png"); // Убедитесь, что путь к изображению правильный
+                Graphics gr = CreateGraphics();
+                gr.DrawImage(backgroundImage, new Rectangle(0, 0, this.Width, this.Height));
+                Refresh();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки изображения: {ex.Message}");
+            }
+        }
+        public Image BackgroundImage
+        {
+            get { return backgroundImage; }
+            set { backgroundImage = value; //Invalidate();
+                                           }
+        }
+
+    }
+}
